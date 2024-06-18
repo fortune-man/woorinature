8734046b9cb1e5d136117705543b3f1adacd46e6
fortune-man <wnguddl96@knou.ac.kr>
2024-06-18 15:07:56 +0900

    Initial commit

---
+++ src/main/java/com/example/woorinature/WoorinatureApplication.java
@@ -0,0 +1,13 @@
+package com.example.woorinature;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoorinatureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WoorinatureApplication.class, args);
+    }
+
+}
