29a59a826f21759e422a9bd87a447fe14d7d0d84
fortune-man <wnguddl96@knou.ac.kr>
2024-06-18 15:19:04 +0900

    feat(EventRepositor): 레포지토리 인터페이스 추가

---
+++ src/main/java/com/example/woorinature/repository/EventRepository.java
@@ -0,0 +1,13 @@
+package com.example.woorinature.repository;
+
+import com.example.woorinature.model.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Repository
+public interface EventRepository extends JpaRepository<Event, Long> {
+    List<Event> findByDate(LocalDate date);
+}
