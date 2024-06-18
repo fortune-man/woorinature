683fc51035ce67544ce14f9068af1b7cd20e9434
fortune-man <wnguddl96@knou.ac.kr>
2024-06-18 15:46:49 +0900

    test(EventRepositoryTest) : 테스트 실패 후 이벤트 객체의 PK를 id로 설정하여 테스트 통과

--- src/main/java/com/example/woorinature/model/Event.java
+++ src/main/java/com/example/woorinature/model/Event.java
@@ -11,6 +11,7 @@ import java.time.LocalDate;
 @Entity
 public class Event {
 
+    @jakarta.persistence.Id
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
---
+++ src/test/java/com/example/woorinature/repository/EventRepositoryTest.java
@@ -0,0 +1,36 @@
+package com.example.woorinature.repository;
+
+import com.example.woorinature.model.Event;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class EventRepositoryTest {
+
+    @Autowired
+    private EventRepository eventRepository;
+
+    @Test
+    public void 이벤트_저장과_조회_테스트() {
+
+        // given
+        String expect = "이번주 일요일 가족 식사 모임";
+        Event event = new Event();
+        event.setTitle("이번주 일요일 가족 식사 모임");
+
+        // when
+        event.setDate(LocalDate.of(2024, 06, 23));
+        eventRepository.save(event);
+
+        // then
+        List<Event> events = eventRepository.findByDate(LocalDate.of(2024, 06, 23));
+        assertFalse(events.isEmpty());
+        assertEquals(expect, events.get(0).getTitle());
+    }
+}
