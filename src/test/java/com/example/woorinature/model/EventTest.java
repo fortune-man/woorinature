48ccb8aff071889f353525618435dffbb248ab3a
fortune-man <wnguddl96@knou.ac.kr>
2024-06-18 15:32:57 +0900

    test(EventTest) : 이벤트 생성 테스트

---
+++ src/test/java/com/example/woorinature/model/EventTest.java
@@ -0,0 +1,26 @@
+package com.example.woorinature.model;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EventTest {
+
+    @Test
+    void 이벤트_생성_테스트() {
+        // given
+        Event event = new Event();
+        String expect = "이번주 일요일 가족 식사 모임";
+
+        // when
+        event.setTitle("이번주 일요일 가족 식사 모임");
+        event.setDate(LocalDate.of(2024, 06, 23));
+
+        // then
+        assertNotNull(event);
+        assertEquals(expect, event.getTitle());
+        assertEquals(LocalDate.of(2024, 06, 23), event.getDate());
+    }
+}
